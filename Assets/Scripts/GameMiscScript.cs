cafab33c3d00da3dde0e36333017aaaf5a5e23e5
PETEroid <selllfocard@gmail.com>
2016-04-09 15:48:59 -0700

    update peter test sceene

---
+++ Assets/Scripts/GameMiscScript.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameMiscScript : MonoBehaviour {
+
+	public Transform crossHair;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position = crossHair.position;
+	}
+}
